4a441e3b88e94fee3795caa42ae5cff644ad2f6b
Daniel McCarthy <danielmccarthy93@yahoo.com>
2015-10-06 20:03:10 -0400

    Alrighty, added the custom randomizer stuff in, was a lot. Let me know if you have any questions.
    TODO:
    1.) Gotta add a menu button leading to the customRandWorkout methods
    2.) Change the color scheme, which I can do
    3.) We may need to change/replace your expandable list adapter, or it might work

---
+++ SrProject/app/src/main/java/com/example/ufl/srproject/RandCustomWorkout.java
@@ -0,0 +1,341 @@
+package com.example.ufl.srproject;
+
+/**
+ * Created by danie_000 on 10/6/2015.
+ */
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.ufl.srproject.db.CustomWorkoutOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class RandCustomWorkout extends ActionBarActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    List<String> exercisePicks = new ArrayList<String>();
+    List<String> exerciseDisplay = new ArrayList<String>();
+    ArrayAdapter<String> arrayAddNewExercise;
+    String[] workout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rand_custom_workout_menu);
+        final Context useMe = this;
+
+        // get the list view
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,int groupPosition, int childPosition, long id) {
+                Toast.makeText(useMe, "Added: " + listDataHeader.get(groupPosition) + " > " + listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
+
+                exercisePicks.add(listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition));
+                exerciseDisplay.add(listDataHeader.get(groupPosition)+ " > " + listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition));
+                placeIntoListView();
+
+                return false;
+            }
+        });
+
+        final ListView displayExercises = (ListView)findViewById(R.id.list);
+        displayExercises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(useMe,"Removed: " + arrayAddNewExercise.getItem(position) + " Position: " + position, Toast.LENGTH_SHORT).show();
+
+                removeFromListView(position);
+            }
+        });
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Chest");
+        listDataHeader.add("Traps");
+        listDataHeader.add("Shoulders");
+        listDataHeader.add("Biceps");
+        listDataHeader.add("Triceps");
+        listDataHeader.add("Wrists");
+        listDataHeader.add("Upper Back");
+        listDataHeader.add("Lower Back");
+        listDataHeader.add("Full Lower Body");
+        listDataHeader.add("Calves");
+        listDataHeader.add("Quads");
+        listDataHeader.add("Gluts");
+        listDataHeader.add("Hamstrings");
+
+        // Adding child data
+        List<String> chest = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.CHEST).length; i++){
+            chest.add(getResources().getStringArray(R.array.CHEST)[i]);
+        }
+
+        List<String> traps = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.TRAPS).length; i++){
+            traps.add(getResources().getStringArray(R.array.TRAPS)[i]);
+        }
+
+
+        List<String> shoulders = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.SHOULDERS).length; i++){
+            shoulders.add(getResources().getStringArray(R.array.SHOULDERS)[i]);
+        }
+
+        List<String> biceps = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.BICEPS).length; i++){
+            biceps.add(getResources().getStringArray(R.array.BICEPS)[i]);
+        }
+
+        List<String> triceps = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.TRICEPS).length; i++){
+            triceps.add(getResources().getStringArray(R.array.TRICEPS)[i]);
+        }
+
+        List<String> wrists = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.WRISTS).length; i++){
+            wrists.add(getResources().getStringArray(R.array.WRISTS)[i]);
+        }
+
+        List<String> upperBack = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.UPPER_BACK).length; i++){
+            upperBack.add(getResources().getStringArray(R.array.UPPER_BACK)[i]);
+        }
+
+        List<String> lowerBack = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.LOWER_BACK).length; i++){
+            lowerBack.add(getResources().getStringArray(R.array.LOWER_BACK)[i]);
+        }
+
+        List<String> totalLowerBody = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.LOWER_BODY).length; i++){
+            totalLowerBody.add(getResources().getStringArray(R.array.LOWER_BODY)[i]);
+        }
+
+        List<String> calves = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.CALVES).length; i++){
+            calves.add(getResources().getStringArray(R.array.CALVES)[i]);
+        }
+
+        List<String> quads = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.QUADS).length; i++){
+            quads.add(getResources().getStringArray(R.array.QUADS)[i]);
+        }
+
+        List<String> gluts = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.GLUTES).length; i++){
+            gluts.add(getResources().getStringArray(R.array.GLUTES)[i]);
+        }
+
+        List<String> hamstrings = new ArrayList<String>();
+        for(int i =0; i < getResources().getStringArray(R.array.HAMSTRINGS).length; i++){
+            hamstrings.add(getResources().getStringArray(R.array.HAMSTRINGS)[i]);
+        }
+
+        listDataChild.put(listDataHeader.get(0), chest); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), traps);
+        listDataChild.put(listDataHeader.get(2), shoulders);
+        listDataChild.put(listDataHeader.get(3), biceps);
+        listDataChild.put(listDataHeader.get(4), triceps);
+        listDataChild.put(listDataHeader.get(5), wrists);
+        listDataChild.put(listDataHeader.get(6), upperBack);
+        listDataChild.put(listDataHeader.get(7), lowerBack);
+        listDataChild.put(listDataHeader.get(8), totalLowerBody);
+        listDataChild.put(listDataHeader.get(9), calves);
+        listDataChild.put(listDataHeader.get(10), quads);
+        listDataChild.put(listDataHeader.get(11), gluts);
+        listDataChild.put(listDataHeader.get(12), hamstrings);
+    }
+
+    public void randWorkout(View view) {
+        // no exercises selected
+        if (exercisePicks.size() < 1) {
+            Toast.makeText(this,"Please select at least one workout or group to randomize", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            int countTotalExercise = exercisePicks.size();
+            String[] sets;
+            String[] repetitions;
+
+            Random r = new Random();
+
+            sets = new String[countTotalExercise];
+            repetitions = new String[countTotalExercise];
+            countTotalExercise--;
+
+            while (countTotalExercise >= 0) {
+                int randSetSelect = r.nextInt(4);
+                int randStringRepetitionSelect = r.nextInt(4);
+                int randIntRepetitionSelect = r.nextInt(2);
+                int randRepetitionArraySelect = r.nextInt(2);
+
+                sets[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.SETS)[randSetSelect]);
+                if (randRepetitionArraySelect == 0 && sets[countTotalExercise].equals("3")) {
+                    repetitions[countTotalExercise] = getResources().getStringArray(R.array.stringRepetitions)[randStringRepetitionSelect];
+                }
+                else {
+                    repetitions[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.intRepetitions)[randIntRepetitionSelect]);
+                }
+
+                countTotalExercise--;
+            }
+
+            setContentView(R.layout.rand_workout_menu);
+
+            workout = new String[exercisePicks.size()];
+            for(int i = 0; i < workout.length; i++)
+            {
+                workout[i] = exercisePicks.get(i) + "\n" + "\t\t\tSets:\t" + sets[i] + "\n" + "\t\t\tRepetitions:\t" + repetitions[i] + "\n";
+            }
+
+            arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, workout);
+            ListView displayExercises = (ListView)findViewById(R.id.exerciseList);
+            displayExercises.setAdapter(arrayAddNewExercise);
+
+            Button saveButton = (Button)findViewById(R.id.saveRandWorkout);
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    saveWorkout(workout);
+                }
+            });
+        }
+    }
+
+    // Called when the user clicks the More Options button
+    public void options (View view) {
+        if (exercisePicks.size() < 1) {
+            Toast.makeText(this,"Please select at least one workout or group to randomize", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String Storing = "";
+
+            for (int i = 0; i < exercisePicks.size(); i++) {
+                Storing += exercisePicks.get(i);
+                Storing += ";";
+            }
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor prefsEditor = prefs.edit();
+
+            String List = prefs.getString("Custom", "");
+            if(List.equals("")) {
+                prefsEditor.putString("Custom", Storing);
+                prefsEditor.commit();
+            }
+            else {
+                prefsEditor.putString("Custom", List + Storing);
+                prefsEditor.commit();
+            }
+
+            Intent intent = new Intent(this, CustomWorkoutOptions.class);
+            startActivity(intent);
+        }
+    }
+
+    // allows user to see exercises picked
+    public void placeIntoListView(){
+        arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, exerciseDisplay);
+        ListView displayExercises = (ListView)findViewById(R.id.list);
+        displayExercises.setAdapter(arrayAddNewExercise);
+    }
+
+    // allows user to remove selected exercises
+    public void removeFromListView(int position){
+        String removeMe = arrayAddNewExercise.getItem(position);
+
+        // removes a specific exercise
+        if (!removeMe.contains(">")) {
+            // Find what group we are looking to remove
+            int truePosition = 0;
+            for (int i = 0; i < listDataHeader.size(); i++) {
+                if (arrayAddNewExercise.getItem(position) == listDataHeader.get(i)){
+                    truePosition = i;
+                    break;
+                }
+            }
+            // Remove all children of a group from the exercisePicks array
+            for(int i = 0; i < listDataChild.get(listDataHeader.get(truePosition)).size(); i++) {
+                for(int j = 0; j < exercisePicks.size(); j++) {
+                    if (i < listDataChild.get(listDataHeader.get(truePosition)).size() && (listDataChild.get(listDataHeader.get(truePosition)).get(i) == exercisePicks.get(j))) {
+                        exercisePicks.remove(j);
+                        i++;
+                        j = -1;
+                    }
+                }
+            }
+        }
+        else {
+            exercisePicks.remove(position);
+        }
+
+        arrayAddNewExercise.remove(removeMe);
+
+        ListView displayExercises = (ListView)findViewById(R.id.list);
+        displayExercises.setAdapter(arrayAddNewExercise);
+    }
+
+    public void saveWorkout(String[] saveWorkout) {
+        String List = "";
+
+        for(int i = 0; i < saveWorkout.length; i++)
+        {
+            List += saveWorkout[i];
+            List += ";";
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("YOURKEY", List);
+        prefsEditor.commit();
+    }
+
+    public void groupAdd(View v) {
+        Toast.makeText(this,"Added: " + listDataHeader.get((int)v.getTag()) + "  At index: " + v.getTag() + "  Of Length : " + listDataChild.get(listDataHeader.get((int)v.getTag())).size(), Toast.LENGTH_SHORT).show();
+
+        // Add all children of a group to the exercisePicks array
+        for(int i = 0; i < listDataChild.get(listDataHeader.get((int)v.getTag())).size(); i++) {
+            exercisePicks.add(listDataChild.get(listDataHeader.get((int)v.getTag())).get(i));
+        }
+
+        // Display the group we added
+        exerciseDisplay.add(listDataHeader.get((int)v.getTag()));
+        arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, exerciseDisplay);
+        ListView displayExercises = (ListView)findViewById(R.id.list);
+        displayExercises.setAdapter(arrayAddNewExercise);
+    }
+}
---
+++ SrProject/app/src/main/java/com/example/ufl/srproject/db/CustomWorkoutOptions.java
@@ -0,0 +1,237 @@
+package com.example.ufl.srproject.db;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.ufl.srproject.R;
+
+import java.util.Random;
+
+public class CustomWorkoutOptions extends ActionBarActivity {
+
+    String[] exercisePicks;
+    String[] sets;
+    String[] repetitions;
+    String[] workout;
+    ArrayAdapter<String> arrayAddNewExercise;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_workout_options_menu);
+    }
+
+    public void randWorkout (View v) {
+
+        Random r = new Random();
+
+        // Pull the exercise picks from SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String List = prefs.getString("Custom", "");
+        if(!List.equals("")) {
+            exercisePicks = List.split(";");
+        }
+
+        // Initialization of these arrays
+        int countTotalExercise = exercisePicks.length;
+        sets = new String[countTotalExercise];
+        repetitions = new String[countTotalExercise];
+        countTotalExercise--;
+
+        while (countTotalExercise >= 0) {
+            // Choose a random number of sets
+            int randSetSelect = r.nextInt(4);
+            // Choose which array to pull the repetitions from
+            int randRepetitionArraySelect = r.nextInt(2);
+            // Needed a string array from values such as "10, 8, 6"
+            int randStringRepetitionSelect = r.nextInt(4);
+            // Needed an int array for values such as "10" and "6", kill me
+            int randIntRepetitionSelect = r.nextInt(2);
+
+            // Cast the int to a string and set the sets to that number... nice wording
+            sets[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.SETS)[randSetSelect]);
+            // If the roll has chosen the string array of repetition values and there are 3 sets, do this
+            if (randRepetitionArraySelect == 0 && sets[countTotalExercise].equals("3")) {
+                repetitions[countTotalExercise] = getResources().getStringArray(R.array.stringRepetitions)[randStringRepetitionSelect];
+            }
+            else {
+                repetitions[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.intRepetitions)[randIntRepetitionSelect]);
+            }
+
+            countTotalExercise--;
+        }
+
+        // Go to the correct view for displaying this information
+        setContentView(R.layout.rand_workout_menu);
+
+        // Assemble the workout array
+        workout = new String[exercisePicks.length];
+        for(int i = 0; i < workout.length; i++)
+        {
+            workout[i] = exercisePicks[i] + "\n" + "\t\t\tSets:\t" + sets[i] + "\n" + "\t\t\tRepetitions:\t" + repetitions[i] + "\n";
+        }
+
+        // Print the sucker to screen
+        arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, workout);
+        ListView displayExercises = (ListView)findViewById(R.id.exerciseList);
+        displayExercises.setAdapter(arrayAddNewExercise);
+
+        // initialize the save button and its listener
+        Button saveButton = (Button)findViewById(R.id.saveRandWorkout);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveWorkout(workout);
+            }
+        });
+
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("Custom", "");
+        prefsEditor.commit();
+    }
+
+    public void additionalExercises(View v) {
+        Intent intent = new Intent(this, AdditionalExerciseOptions.class);
+        startActivity(intent);
+    }
+
+    public void randNumWorkout (View v) {
+
+        // Pop up dialog
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("How many exercises?");
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        alert.setView(input);
+
+        alert.setPositiveButton("Go!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                // Pull the exercise picks from SharedPreferences
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(CustomWorkoutOptions.this);
+                String List = prefs.getString("Custom", "");
+                if(!List.equals("")) {
+                    exercisePicks = List.split(";");
+                }
+
+                int numberOfExercises = Integer.parseInt(input.getText().toString());
+
+                if(numberOfExercises > exercisePicks.length) {
+                    Toast.makeText(CustomWorkoutOptions.this,"Can't randomize this many exercises: List is to small", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Random r = new Random();
+
+                    // Initialization of these arrays
+                    int countTotalExercise = numberOfExercises;
+                    String [] tempExercises = new String[countTotalExercise];
+                    sets = new String[countTotalExercise];
+                    repetitions = new String[countTotalExercise];
+                    countTotalExercise--;
+
+                    while (countTotalExercise >= 0) {
+                        // Choose  a random exercise
+                        int randExerciseSelect = r.nextInt(exercisePicks.length);
+                        // Choose a random number of sets
+                        int randSetSelect = r.nextInt(4);
+                        // Choose which array to pull the repetitions from
+                        int randRepetitionArraySelect = r.nextInt(2);
+                        // Needed a string array from values such as "10, 8, 6"
+                        int randStringRepetitionSelect = r.nextInt(4);
+                        // Needed an int array for values such as "10" and "6", kill me
+                        int randIntRepetitionSelect = r.nextInt(2);
+
+                        // Check if the exercise has already been selected and placed into the workout
+                        for (int i = 0; i < tempExercises.length; i++) {
+                            if (tempExercises[i] == exercisePicks[randExerciseSelect]) {
+                                // If so, re-roll and restart the check
+                                randExerciseSelect = r.nextInt(exercisePicks.length);
+                                i = -1;
+                            }
+                        }
+                        // If it passes the check, put it into the workout
+                        tempExercises[countTotalExercise] = exercisePicks[randExerciseSelect];
+
+                        // Cast the int to a string and set the sets to that number... nice wording
+                        sets[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.SETS)[randSetSelect]);
+                        // If the roll has chosen the string array of repetition values and there are 3 sets, do this
+                        if (randRepetitionArraySelect == 0 && sets[countTotalExercise].equals("3")) {
+                            repetitions[countTotalExercise] = getResources().getStringArray(R.array.stringRepetitions)[randStringRepetitionSelect];
+                        } else {
+                            repetitions[countTotalExercise] = Integer.toString(getResources().getIntArray(R.array.intRepetitions)[randIntRepetitionSelect]);
+                        }
+
+                        countTotalExercise--;
+                    }
+
+                    // Go to the correct view for displaying this information
+                    setContentView(R.layout.rand_workout_menu);
+
+                    // Assemble the workout array
+                    workout = new String[tempExercises.length];
+                    for (int i = 0; i < workout.length; i++) {
+                        workout[i] = tempExercises[i] + "\n" + "\t\t\tSets:\t" + sets[i] + "\n" + "\t\t\tRepetitions:\t" + repetitions[i] + "\n";
+                    }
+
+                    // Print the sucker to screen
+                    arrayAddNewExercise = new ArrayAdapter<String>(CustomWorkoutOptions.this, R.layout.simple_list_item_custom, workout);
+                    ListView displayExercises = (ListView) findViewById(R.id.exerciseList);
+                    displayExercises.setAdapter(arrayAddNewExercise);
+
+                    // initialize the save button and its listener
+                    Button saveButton = (Button) findViewById(R.id.saveRandWorkout);
+                    saveButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            saveWorkout(workout);
+                        }
+                    });
+
+                    SharedPreferences.Editor prefsEditor = prefs.edit();
+                    prefsEditor.putString("Custom", "");
+                    prefsEditor.commit();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        dialog.show();
+    }
+
+    public void saveWorkout(String[] saveWorkout) {
+        String List = "";
+
+        for(int i = 0; i < saveWorkout.length; i++)
+        {
+            List += saveWorkout[i];
+            List += ";";
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("YOURKEY", List);
+        prefsEditor.commit();
+    }
+}
