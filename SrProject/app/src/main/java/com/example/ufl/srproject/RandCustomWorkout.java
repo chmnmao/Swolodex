9bf45958b5a6a4502085b693d34f28c167a92dcd
Kevin Liu <kevinliu314@gmail.com>
2015-10-13 21:42:33 -0400

    finished reformatting and tweaking customize workout page todo next: play with the expandable list to make it look better

--- SrProject/app/src/main/java/com/example/ufl/srproject/RandCustomWorkout.java
+++ SrProject/app/src/main/java/com/example/ufl/srproject/RandCustomWorkout.java
@@ -7,9 +7,17 @@ package com.example.ufl.srproject;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -37,10 +45,103 @@ public class RandCustomWorkout extends ActionBarActivity {
     ArrayAdapter<String> arrayAddNewExercise;
     String[] workout;
 
+    //region TOOLBAR VARIABLE DECLARATION
+    //TODO: Store this information in res xml files
+    //First we declare icons and titles for navigation
+    //Store them in the array below
+    String TITLES[] = {"Home","Events","Mail","Shop","Travel","Customize"};
+    //TODO: Uniform icon styles
+    int ICONS[] = {R.drawable.ic_home,R.drawable.ic_events,R.drawable.ic_mail,R.drawable.ic_shop,R.drawable.ic_travel,R.drawable.ic_cust_weight};
+
+    //Add string resources for profile data that we also store in the header
+    //And we also create a int resource for profile picture
+    String NAME = "Test Tester";
+    String EMAIL = "test@test.com";
+    int PROFILE = R.drawable.gator;
+
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;                  // Declaring Action Bar Drawer Toggle
+//endregion
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rand_custom_workout_menu);
+        Resources res = getResources();
+
+        //region INITIALIZE TOOLBAR AND MENU NAVIGATION
+        /* Assinging the toolbar object ot the view
+    and setting the the Action bar to our toolbar
+     */
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+        //region Setting onclick listeners for the RecyclerView
+        final GestureDetector gd = new GestureDetector(RandCustomWorkout.this,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onSingleTapUp(MotionEvent e){
+                return true;
+            }
+        });
+        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener(){
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent){
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(),motionEvent.getY());
+                if(child!=null&&gd.onTouchEvent(motionEvent)){
+                    Drawer.closeDrawers();
+                    //Do something here depending on item
+                    if(recyclerView.getChildPosition(child)==6){
+                        //We have clicked on the customize workout
+                        //We're in the custom workout do nothing
+                    }
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent){}
+        });
+        //endregion
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.mainLayout);        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+        };
+        // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
+//endregion
+
         final Context useMe = this;
 
         // get the list view
@@ -69,7 +170,7 @@ public class RandCustomWorkout extends ActionBarActivity {
             }
         });
 
-        final ListView displayExercises = (ListView)findViewById(R.id.list);
+        final ListView displayExercises = (ListView)findViewById(R.id.selectionList);
         displayExercises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -269,8 +370,8 @@ public class RandCustomWorkout extends ActionBarActivity {
 
     // allows user to see exercises picked
     public void placeIntoListView(){
-        arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, exerciseDisplay);
-        ListView displayExercises = (ListView)findViewById(R.id.list);
+        arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.item_list_view_swolodex_2, exerciseDisplay);
+        ListView displayExercises = (ListView)findViewById(R.id.selectionList);
         displayExercises.setAdapter(arrayAddNewExercise);
     }
 
@@ -305,7 +406,7 @@ public class RandCustomWorkout extends ActionBarActivity {
 
         arrayAddNewExercise.remove(removeMe);
 
-        ListView displayExercises = (ListView)findViewById(R.id.list);
+        ListView displayExercises = (ListView)findViewById(R.id.selectionList);
         displayExercises.setAdapter(arrayAddNewExercise);
     }
 
@@ -335,7 +436,7 @@ public class RandCustomWorkout extends ActionBarActivity {
         // Display the group we added
         exerciseDisplay.add(listDataHeader.get((int)v.getTag()));
         arrayAddNewExercise = new ArrayAdapter<String>(this, R.layout.simple_list_item_custom, exerciseDisplay);
-        ListView displayExercises = (ListView)findViewById(R.id.list);
+        ListView displayExercises = (ListView)findViewById(R.id.selectionList);
         displayExercises.setAdapter(arrayAddNewExercise);
     }
 }
